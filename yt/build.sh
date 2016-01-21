663f2f6225e47022243a4729dbcec546b7e58cfb
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-01-21 16:13:05 -0600

    yt: Add simple test file

--- yt/build.sh
+++ yt/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export PNG_DIR=$PREFIX
-export FTYPE_DIR=$PREFIX
-export HDF5_DIR=$PREFIX
 
-echo $PYTHON
+echo "$PREFIX" > embree.cfg
 $PYTHON setup.py install
+
+cp scripts/* $PREFIX/bin/
+
+sed -n 's/^__version__ = "\(.*\)"$/\1/p' ${SRC_DIR}/yt/__init__.py > __conda_version__.txt
+sed -i -e 's/-/_/g' __conda_version__.txt
+export HG_LASTTAG_REV=$(hg id -r  'sort(tag(), date) and branch(stable)' -i)
+echo $(hg log -r "${HG_LASTTAG_REV}:  and branch(yt)" | grep -c changeset) > __conda_buildnum__.txt

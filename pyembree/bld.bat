82bd41c942fac424e59b304f6d14213a55552889
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-01-23 14:56:57 -0600

    pyembree: shuffle libs for win cause it ignores env vars...

--- pyembree/bld.bat
+++ pyembree/bld.bat
@@ -1,6 +1,9 @@
-set INCLUDE=%LIBRARY_INC%
+set CPATH=%LIBRARY_INC%
+set LIBRARY_PATH=%LIBRARY_LIB%
 set LIBPATH=%LIBRARY_LIB%
 set LIB=%LIBRARY_LIB%
+
+copy %LIBRARY_LIB%\*.dll %LIBRARY_LIB%\..\..\libs\
 
 "%PYTHON%" setup.py build --compiler=mingw32 install
 if errorlevel 1 exit 1

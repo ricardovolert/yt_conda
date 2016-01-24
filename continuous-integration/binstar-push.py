547d2382174d0640c71afea40e916f3894b81e07
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-01-24 13:15:09 -0600

    Add appveyor stub, based on https://github.com/rmcgibbo/python-appveyor-conda-example/

---
+++ continuous-integration/binstar-push.py
@@ -0,0 +1,14 @@
+import os
+import glob
+import subprocess
+import traceback
+
+exit()
+token = os.environ['BINSTAR_TOKEN']
+cmd = ['binstar', '-t', token, 'upload', '--force']
+cmd.extend(glob.glob('*.tar.bz2'))
+try:
+    subprocess.check_call(cmd)
+except subprocess.CalledProcessError:
+    traceback.print_exc()
+

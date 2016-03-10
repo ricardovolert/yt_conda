26d4cf50df069ec6ab03a7d2886b24e72cdd04be
Kacper Kowalik <xarthisius.kk@gmail.com>
2016-03-10 11:13:45 -0600

    Add cyglfw3 and glfw3

---
+++ glfw3/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir build
+cd build
+
+CMAKE_GENERATOR="Unix Makefiles"
+CMAKE_ARCH="-m$ARCH"
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  export CFLAGS="$CFLAGS $CMAKE_ARCH"
+  export LDLAGS="$LDLAGS $CMAKE_ARCH"
+fi
+
+cmake .. "-G$CMAKE_GENERATOR" \
+	-DBUILD_SHARED_LIBS=1 \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX
+
+make
+make install
